629ca3ab5775f91ac2d2978e19cec0287cde1c19
jeyn99 <jane.repollo@student.passerellesnumeriques.org>
2019-08-28 17:39:07 +0800

    Still updating for function

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,20 +1,73 @@
 // basic functionalities
-client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
-client.subscribe("mqtt/demo")
+$(document).ready(function () {
+  client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
+  $("#add").val("ws://broker.hivemq.com:8000/mqtt");
+  var stats;
+  var top = $("#topicPub");
+  var pload = $("#payload");
+  var subscribeTopic = $("#topicSub");
+  var time = new Date($.now());
 
-client.on("connect", function(){
-    console.log("Successfully connected");
-})
+  client.on("connect", function () {
+    stats = ("Successfully connected");//display in console to check its connectivity
+  })
 
-client.on("message", function (topic, payload) {
-  console.log([topic, payload].join(": "));
-  client.end();
-})
+  $("#connectBtn").click(function (e) {
+    e.preventDefault();
+    console.log("Connect");
+    console.log(time.toUTCString());
+    $("#status").val(stats);//connection display
+    
 
-client.publish("mqtt/demo", "hello world!")
+    ($("#pubBtn").click(function () {
+      client.publish(top.val(), pload.val());
+      //$("#PublishDetails").$("<tr><td>" + top.val())
+    }))
+
+
+    $("#subBtn").click(function () {
+      client.subscribe(subscribeTopic.val())
+      client.publish(top.val(), pload.val());
+      client.on("message", function (topic, payload) {
+        console.log([topic, payload].join(": "));
+        
+      })
+      var row = "<tr><td>" + top.val() + "</td><td>" + time.toUTCString() + "</td></tr>";
+      $("#SubDetails").append(row);
+    })
+
+    $("#unsubBtn").click(function () {
+      client.subscribe(subscribeTopic.val())
+      client.publish(top.val(), pload.val());
+      client.on("message", function (topic, payload) {
+        console.log([topic, payload].join(": "));
+        client.end();
+      })
+      
+    })
+
+  });
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
 // // advance functionalities
+
 // client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
+
 // client.subscribe("mqtt/demo", function (err){
 //   if (err){
 //     console.log(err);
