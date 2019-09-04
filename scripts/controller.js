463a008c84afa73f36ed469615c7175b9cfb5a3c
jeyn99 <jane.repollo@student.passerellesnumeriques.org>
2019-09-04 15:08:50 +0800

    Function to be checked

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,55 +1,52 @@
 // basic functionalities
 $(document).ready(function () {
-  client = mqtt.connect("wss://test.mosquitto.org:8081/mqtt")
-
-  var brokweAdd = "wss://test.mosquitto.org:8081/mqtt";
+  var brokweAdd = "ws://broker.hivemq.com:8000/mqtt";
   $("#add").val(brokweAdd);
-  var stats;
   var top = $("#topicPub");
   var pload = $("#payload");
   var subscribeTopic = $("#topicSub");
   var time = new Date($.now());
 
 
-  client.on("connect", function () {
-    stats = ("Successfully connected");//display in console to check its connectivity
-  })
-
-
   $("#connectBtn").click(function (e) {
     e.preventDefault();
-    $("#status").val(stats);
+    $("#status").val("Connecting...");
 
-    ($("#pubBtn").click(function () {
-      client.publish(top.val(), pload.val());
-      $("#PubDetails").show();
-      var row = "<tr><td>" + top.val() + "</td><td>" + pload.val() + "</td><td>" + time.toUTCString() + "</td></tr>";
-      $("#tb").append(row);
-    }))
+    client = mqtt.connect(brokweAdd);
+    client.on("connect", function () {
+      $("#status").val("Successfully Connected");
 
-    $("#subBtn").click(function () {
-      client.subscribe(subscribeTopic.val())
-      //client.publish(top.val(), pload.val());
-      $("#SubDetails").show();
-      var row = "<tr><td>" + subscribeTopic.val() + "</td><td>" + time.toUTCString() + "</td></tr>";
-      $("#tbsub").append(row);
-      client.on("message", function (topic, payload) {
-        console.log([topic, payload].join(": "));
-        $("#SubsPubDetails").show();
-        var row = "<tr><td>" + subscribeTopic.val() + "</td><td>" + payload + "</td><td>" + time.toUTCString() + "</td></tr>";
-        $("#tbsubs").append(row);
+      ($("#pubBtn").click(function () {
+        client.publish(top.val(), pload.val());
+        $("#PubDetails").show();
+        var row = "<tr><td>" + top.val() + "</td><td>" + pload.val() + "</td><td>" + time.toUTCString() + "</td></tr>";
+        $("#tb").append(row);
+      }))
+
+      $("#subBtn").click(function () {
+        client.subscribe(subscribeTopic.val())
+        //client.publish(top.val(), pload.val());
+        $("#SubDetails").show();
+        var row = "<tr><td>" + subscribeTopic.val() + "</td><td>" + time.toUTCString() + "</td></tr>";
+        $("#tbsub").append(row);
+        client.on("message", function (topic, payload) {
+          console.log([topic, payload].join(": "));
+          $("#SubsPubDetails").show();
+          var row = "<tr><td>" + subscribeTopic.val() + "</td><td>" + payload + "</td><td>" + time.toUTCString() + "</td></tr>";
+          $("#tbsubs").append(row);
+        })
       })
-    })
 
-    $("#disconnectBtn").click(function () {
-      //client.end();
-      location.reload();
-      //$("input").val(null);
-      //$("#add").val(brokweAdd);
-    })
+      $("#disconnectBtn").click(function () {
+        //client.end();
+        location.reload();
+        //$("input").val(null);
+        //$("#add").val(brokweAdd);
+      })
 
-    $("#unsubBtn").click(function () {
-      client.unsubscribe(subscribeTopic.val())
+      $("#unsubBtn").click(function () {
+        client.unsubscribe(subscribeTopic.val())
+      })
     })
   })
 });
